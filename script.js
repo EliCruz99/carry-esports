0bd53b4bd1e5b5e4fb52f89146003355b2b8e379
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-06-23 09:38:44 -0700

    finished basic async and updated minor css

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
-const DOMAIN = "https://api.pandascore.co/";
+// Game arrays
+// series Dota 2 
+// LEAGUES ESL CSGO leagues 21
+// LEAGUES lcs[27] lec[28] LPL[9] LCK[64] LOL
+// LEAGUES cod [0] codLeague
+// LEAGUES valorant champ tours [9]NA[35] EU[34]
+// LEAGUES ow [5]
+
+const DOMAIN = "https://agile-lake-43212.herokuapp.com/api.pandascore.co";
 const TOKEN = "eatsq_09CwuFxH5ZuhF65XB0EGLMdNl9HTtDMS01g9qpakQItnc"
 const searchBar = document.querySelector('#game-search')
 const btn = document.querySelector('#Search')
+const upcoming1 = document.querySelector('.upcoming1')
+const leagueOfLegends = "lol"
+const callOfDuty = "codmw"
+const counterStrike = "csgo"
+const valorant = "valorant"
+const overWatch = "ow"
 
 
-async function tourneyData(game) {
+btn.addEventListener('click', () => {
+  // leagueData(searchBar.value)
+  seriesData(searchBar.value)
+  searchBar.value = ""
+})
+
+async function leagueData(game) {
   try {
-    const response = await axios.get(`https://api.pandascore.co/tournaments?token=${TOKEN}`)
-    console.log(response)
+    const response = await axios.get(`${DOMAIN}/${game}/leagues?page[size]=100&token=${TOKEN}`)
+    for (let i = 0; i < response.data.length; i++) {
+      console.log(response.data[i])
+      
+    }
+    
+  } catch (error) {
+    console.error(error)
+  }
+}
+async function seriesData(game) {
+  try {
+    const response = await axios.get(`${DOMAIN}/${game}/series?page[size]=100&token=${TOKEN}`)
+    // console.log(response.data)
+    for (let i = 0; i < response.data.length; i++) {
+        console.log(response.data[i])
+    }
     
   } catch (error) {
     console.error(error)
   }
 }
 
-// tourneyData('csgo')
-btn.addEventListener('click', () => {
-  console.log(searchBar.value)
-  searchBar.value = ""
-})
+
+
+function renderTounreyList(game) {
+    for (let i = 0; i < game.length; i++) {
+      
+      const games = document.createElement('h3')
+      games.textContent = game[i].league
+      upcoming1.append(games)
+      
+      const stats = document.createElement('p')
+      stats.textContent = game[i]
+      
+    }
+}
